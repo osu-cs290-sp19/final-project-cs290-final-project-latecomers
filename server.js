e9ff74375835f69fb4cce8be7677a3e1f579270d
Miklos Bowling <bowlingm@oregonstate.edu>
2019-06-05 22:43:04 -0700

    Basic framework

---
+++ server.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
