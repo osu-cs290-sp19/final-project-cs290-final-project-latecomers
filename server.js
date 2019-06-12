4c61541cb36c06f178a2bc03fd81548832653a9d
Samuel-Fox <43558940+Samuel-Fox@users.noreply.github.com>
2019-06-11 19:54:43 -0700

    Handlebars set up

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphbs());
+var storyData = require('./storyData');
+console.log("== storyData", storyData);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+app.get('/', function (req, res) {
+	res.status(200).render('storyPage', {storyData});
+});
+
 app.get('*', function (req, res) {
-  res.status(404).render('404');
+  res.status(404).render('404Page');
 });
 
 app.listen(port, function () {
